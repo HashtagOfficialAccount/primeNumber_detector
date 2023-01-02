8552e54729dc93cd42471a11d56beb534f4f4c8c
Srinikash Venki <75814786+HashtagOfficialAccount@users.noreply.github.com>
2023-01-01 19:04:47 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,14 @@
-#user gets the opportunity enter a number (prime or composite)
-num = int(input('Enter your number: '))
-
-#all the even numbers except 2 
-if num % 2 == 0 and num != 2: 
-    print('Your number is NOT a prime number')  
-    exit()
-
-if num % 3 == 0 and num != 3:  
-    print('Your number is NOT a prime number')  
-    exit()
-
-
-if num % 5 == 0 and num != 5:  
-    print('Your number is NOT a prime number')  
-    exit()
+num = int(input("Enter a number to checked for prime: "))
+if num <= 1:
+    print("Your number is neither prime or composite")
+else:
+    index = 0
+    for i in range(1,num + 1):
+        if num % i == 0:
+            index += 1
+    if index == 2:
+        print("Your number is prime")
+    else:
+        print("Your number is composite")
 
 
-if num % 7 == 0 and num != 7:  
-    print('Your number is NOT a prime number')  
-    exit()
-
-
-if num % 9 == 0 and num != 9:  
-    print('Your number is NOT a prime number')  
-    exit()
-
-print('Your number IS a prime number')
